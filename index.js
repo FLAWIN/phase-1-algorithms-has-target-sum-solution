7f2ec821685e27204fef775b18911b463e79f31e
Aloe Flawin <aloeflawingulenywa98@gmail.com>
2023-10-05 14:02:25 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    // n steps (depending on the length of the input array)
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      // n * n steps (nested loop!)
+      if (array[j] === complement) return true;
+    }
+  }
+  // 1 step
+  return false;
 }
+
+
+/* CLEAN AND READABLE CODE
+// function hasTargetSum(array, target) {
+//   for (let i = 0; i < array.length; i++) {
+//     const complement = target - array[i];
+//     for (let j = i + 1; j < array.length; j++) {
+//       if (array[j] === complement) return true;
+//     }
+//   }
+//   return false;
+// }
+*/
+
+/* THE CODE
+// function hasTargetSum(array, target) {
+//   // iterate over the array of numbers
+//   for (let i = 0; i < array.length; i++) {
+//     // for the current number, identify a complementary number that adds to our target
+//     // (for example: if our number is 2, and the target is 5, the complementary number is 3)
+//     const complement = target - array[i];
+//     // iterate over the remaining numbers in the array
+//     for (let j = i + 1; j < array.length; j++) {
+//       // check if any of the remaining numbers is the complement
+//       // if so, return true
+//       if (array[j] === complement) return true;
+//     }
+//   }
+//   // if we reach the end of the array, return false
+//   return false;
+// }
+*/
 
 /* 
   Write the Big O time complexity of your function here
@@ -10,25 +52,28 @@ function hasTargetSum(array, target) {
   Add your pseudocode here
 */
 
+// function hasTargetSum(array, target) {
+//   // iterate over the array of numbers
+//   //   for the current number, identify a complementary number that adds to our target
+//   //   (for example: if our number is 2, and the target is 5, the complementary number is 3)
+//   //   iterate over the remaining numbers in the array
+//   //     check if any of the remaining numbers is the complement
+//   //       if so, return true
+//   // if we reach the end of the array, return false
+// }
 /*
   Add written explanation of your solution here
 */
-
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
-
   console.log("");
-
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
-
   console.log("");
-
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
-
-module.exports = hasTargetSum;
+module.exports = hasTargetSum
